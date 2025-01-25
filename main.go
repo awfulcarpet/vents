cb693d89301e01353b5840846ad75b47a585aef0
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-01-25 01:00:16 -0700

    post for vents

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -20,11 +21,11 @@ type Vent struct {
 
 func VentsHandler(w http.ResponseWriter, r *http.Request) {
 	b, _ := os.ReadFile("vents")
+	var vents Vents
+	json.Unmarshal(b, &vents)
 
 	switch r.Method {
 	case "GET": {
-		var vents Vents
-		json.Unmarshal(b, &vents)
 
 		for i := 0; i < len(vents.Vents); i++ {
 			time := time.Unix(vents.Vents[i].Date, 0);
@@ -33,6 +34,19 @@ func VentsHandler(w http.ResponseWriter, r *http.Request) {
 				time.Hour(), time.Minute());
 			fmt.Fprintf(w, "%s\n", vents.Vents[i].Content);
 		}
+	}
+	case "POST": {
+		body, _ := io.ReadAll(r.Body)
+		new := &Vent {
+			Date: time.Now().Unix(),
+			Content: string(body),
+		}
+
+		vents.Vents = append(vents.Vents, *new)
+
+		json, _ := json.Marshal(vents);
+
+		os.WriteFile("vents", json, 0644);
 	}
 	default:
 		http.Error(w, "Request Invalid", http.StatusMethodNotAllowed);
@@ -70,5 +84,6 @@ func main() {
 
 
 	fmt.Printf("listening on port %d\n", port);
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil));
 }
